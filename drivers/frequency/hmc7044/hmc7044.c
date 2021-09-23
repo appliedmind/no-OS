835f69daaf7721cfe389e09bf7fdcb718bea9be7
Aaron Holtzman <aaron@appliedmind.ca>
2021-09-23 18:27:07 +0000

    hmc7044: fix clk_set_rate if channel numbers aren't continuous
    Previously this function treated the channel number as an index into the channel list, which worked as long as the channel list contained continuous channel numbers starting at zero. This fix looks the channel number up in the list and returns an error if not found.

--- drivers/frequency/hmc7044/hmc7044.c
+++ drivers/frequency/hmc7044/hmc7044.c
@@ -357,28 +357,36 @@ int32_t hmc7044_clk_round_rate(struct hmc7044_dev *dev, uint32_t rate,
 /**
  * Set channel rate.
  * @param dev - The device structure.
- * @param chan - Channel number.
+ * @param chan_num - Channel number.
  * @param rate - Channel rate.
  * @return SUCCESS in case of success, negative error code otherwise.
  */
-int32_t hmc7044_clk_set_rate(struct hmc7044_dev *dev, uint32_t chan,
+int32_t hmc7044_clk_set_rate(struct hmc7044_dev *dev, uint32_t chan_num,
 			     uint64_t rate)
 {
 	uint32_t div;
 	int32_t ret;
+	struct hmc7044_chan_spec *chan = NULL;
 
-	if (chan >= dev->num_channels)
+	/* Find the reqested channel number */
+	for (int i=0; i < dev->num_channels; i++) {
+		if (dev->channels[i].num == chan_num) {
+			chan = &dev->channels[i];
+			break;
+		}
+	}
+	if (chan == NULL )
 		return FAILURE;
 
 	div = hmc7044_calc_out_div(rate, dev->pll2_freq);
-	dev->channels[chan].divider = div;
+	chan->divider = div;
 
-	ret = hmc7044_write(dev, HMC7044_REG_CH_OUT_CRTL_1(chan),
+	ret = hmc7044_write(dev, HMC7044_REG_CH_OUT_CRTL_1(chan->num),
 			    HMC7044_DIV_LSB(div));
 	if(ret < 0)
 		return ret;
 
-	return hmc7044_write(dev, HMC7044_REG_CH_OUT_CRTL_2(chan),
+	return hmc7044_write(dev, HMC7044_REG_CH_OUT_CRTL_2(chan->num),
 			     HMC7044_DIV_MSB(div));
 }
 
@@ -602,6 +610,7 @@ static int32_t hmc7044_setup(struct hmc7044_dev *dev)
 		if (chan->num >= HMC7044_NUM_CHAN || chan->disable)
 			continue;
 
+		printf("hmc7044: programming chan:%d divider:%d\n", chan->num, chan->divider);
 		hmc7044_write(dev, HMC7044_REG_CH_OUT_CRTL_1(chan->num),
 			      HMC7044_DIV_LSB(chan->divider));
 		hmc7044_write(dev, HMC7044_REG_CH_OUT_CRTL_2(chan->num),
